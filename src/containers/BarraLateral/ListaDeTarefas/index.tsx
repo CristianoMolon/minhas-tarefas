351c6765f981396cb4f7bcad5b653f1b856069f0
Cristiano <cristianosilvamolon@gmail.com>
2024-06-19 15:39:08 -0300

    criando a troca de botoes

--- src/containers/BarraLateral/ListaDeTarefas/index.tsx
+++ src/containers/BarraLateral/ListaDeTarefas/index.tsx
@@ -1,19 +1,41 @@
 import Tarefa from '../../../components/FiltroCard/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula da ebac',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Estudar Sass',
+    descricao: 'Ver a aula da ebac',
+    prioridade: 'normal',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Estudar Python',
+    descricao: 'Ver a aula da ebac',
+    prioridade: 'urgente',
+    status: 'concluída'
+  }
+]
+
 const ListaDetarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            status={t.status}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
