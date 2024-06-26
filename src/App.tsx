0cd8012f252d73dde1bf0c467c6b351c66a63807
Cristiano <cristianosilvamolon@gmail.com>
2024-06-26 15:06:55 -0300

    criando rotas com o react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import EstiloGlobal, { Container } from './Styles'
-import BarraLateral from './containers/BarraLateral'
-import ListaDetarefas from './containers/ListaDeTarefas'
 import store from './Store'
+import Home from './pages/Home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova tarefa</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDetarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
